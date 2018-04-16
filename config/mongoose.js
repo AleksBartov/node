8cc59483ca40c66160b8e08a1e81d8dd2548dcc4
Aleksandr <aleksandrbartov@yandex.ru>
2018-04-17 01:57:36 +0400

    mongodb

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const config = require('./config'),
+    mongoose = require('mongoose');
+
+module.exports = function() {
+  const db = mongoose.connect(config.db);
+
+  require('../app/models/user.server.model');
+
+  return db;
+};
